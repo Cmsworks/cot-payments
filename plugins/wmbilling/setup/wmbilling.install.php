4e7b67764ebae1382a717fe4a1499bd3a6685bb0
cmsworksru <github@cmsworks.ru>
2013-08-04 09:41:47 +0400

    fixed #1

--- plugins/wmbilling/setup/wmbilling.install.php
+++ plugins/wmbilling/setup/wmbilling.install.php
@@ -13,6 +13,8 @@ defined('COT_CODE') or die('Wrong URL');
 
 require_once cot_incfile('payments', 'module');
 
+global $db_payments;
+
 if (!$db->fieldExists($db_payments, "pay_wmrnd"))
 {
 	$db->query("ALTER TABLE `$db_payments` ADD COLUMN `pay_wmrnd` varchar(255) NOT NULL");
